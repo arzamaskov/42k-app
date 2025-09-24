4b2ccb871c2dfa947d1d551489f8effd8214b04a
Andrey Arzamaskov <a.arzamaskov@kt.team>
2025-09-24 21:43:17 +0400

    feat(runs): register RunRepository binding in RunsServiceProvider

---
+++ app/app/Runs/Infrastructure/Database/Eloquent/Repositories/EloquentRunRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Runs\Infrastructure\Database\Eloquent\Repositories;
+
+class EloquentRunRepository
+{
+
+}
--- app/app/Runs/RunsServiceProvider.php
+++ app/app/Runs/RunsServiceProvider.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace App\Runs;
 
+use App\Runs\Domain\Contracts\RunRepository;
+use App\Runs\Infrastructure\Database\Eloquent\Repositories\EloquentRunRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RunsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->app->bind(
+            RunRepository::class,
+            EloquentRunRepository::class
+        );
     }
 
     public function boot(): void
